384a88f29296b530f013257b9736719f41d6d120
SaurabhPatel02 <saurabh.patel0@gmail.com>
2021-12-20 04:30:46 +0530

    Added all code files at a single location

---
+++ Code/API_As_Source.py
@@ -0,0 +1,71 @@
+import requests
+#  https://www.alphavantage.co/documentation/
+# test first api to get current iss location
+api11_data = requests.get("http://api.open-notify.org/iss-now.json")
+print(api11_data)
+print(api11_data.json())
+
+parsed_api11_data = api11_data.json()
+print(type(parsed_api11_data))
+print(parsed_api11_data["iss_position"])
+print(parsed_api11_data["message"])
+
+# test first api to find number of person in space
+api12_data = requests.get("http://api.open-notify.org/astros.json")
+print(api12_data)
+print(api12_data.json())
+parsed_api12_data = api12_data.json()
+print(type(parsed_api12_data))
+print(parsed_api12_data["number"])
+print(parsed_api12_data["message"])
+print(parsed_api12_data["people"])
+# print multiple values
+for p in parsed_api12_data["people"]:
+    print(p["name"])
+for p in parsed_api12_data["people"]:
+    print(p["craft"])
+
+# api key CIBFBXPX8IO5DC5M
+
+# implement type 2 API - stock - intraday trading with key
+api2_data = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=CIBFBXPX8IO5DC5M")
+print(api2_data)
+print(api2_data.json())
+parsed_api2_data = api2_data.json()
+print(type(parsed_api2_data))
+print(parsed_api2_data["Meta Data"])
+print(parsed_api2_data["Time Series (5min)"])
+
+# implement type 2 API - company overview with key
+api22_data = requests.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=CIBFBXPX8IO5DC5M")
+print(api22_data)
+print(api22_data.json())
+parsed_api22_data = api22_data.json()
+print(type(parsed_api22_data))
+print(parsed_api22_data["Symbol"])
+print(parsed_api22_data["AssetType"])
+print(parsed_api22_data["ProfitMargin"])
+
+# implement type 2 API - inflation with key
+api23_data = requests.get("https://www.alphavantage.co/query?function=INFLATION&apikey=CIBFBXPX8IO5DC5M")
+print(api23_data)
+print(api23_data.json())
+parsed_api23_data = api23_data.json()
+print(type(parsed_api23_data))
+print(parsed_api23_data["name"])
+print(parsed_api23_data["data"])
+# print all dates for which inflation rate is available
+for p in parsed_api23_data["data"]:
+    print(p["date"])
+
+# implement type 2 API - inflation expectation with key
+api24_data = requests.get("https://www.alphavantage.co/query?function=INFLATION_EXPECTATION&apikey=CIBFBXPX8IO5DC5M")
+print(api24_data)
+print(api24_data.json())
+parsed_api24_data = api24_data.json()
+print(type(parsed_api24_data))
+print(parsed_api24_data["name"])
+print(parsed_api24_data["data"])
+# print all dates for which inflation rate is available
+for p in parsed_api24_data["data"]:
+    print(p["date"])
---
+++ Code/CSV_As_Source_With_Data_Cleaning.py
@@ -0,0 +1,71 @@
+import pandas as pd
+
+netflixdata = pd.read_csv("C:/Users/BYO/PycharmProjects/UCDPA_SaurabhPatel/Data/netflix_titles.csv")
+#print(netflixdata)
+#print (netflixdata.info())
+#print (netflixdata.describe())
+
+#read from local machine different folder
+#netdata=pd.read_csv("C:/Users/BYO/PycharmProjects/netflix_titles_diffloc.csv")
+#print(netdata)
+#print(netdata.head())
+#print(netdata.shape)
+
+#print missing value
+#missingval = netflixdata.isna()
+#print(missingval)
+#missingval = netflixdata.isna().sum()
+#print(missingval)
+
+#handle missing value
+#delete all missing value (default delete from row - axis=0)
+#cleaned_data=netflixdata.dropna()
+#print(netflixdata.shape,cleaned_data.shape)
+
+#delete all column missing value
+#cleaned_data=netflixdata.dropna(axis=1)
+#print(netflixdata.shape,cleaned_data.shape)
+
+#delete all missing value by replacing with other value
+#cleaned_data=netflixdata.fillna("*")
+#print(netflixdata.shape,cleaned_data.shape)
+#missingval= cleaned_data.isna().sum()
+#print(cleaned_data["director"])
+#print(missingval)
+
+#delete all missing value by replacing with mean value
+#cleaned_data=netflixdata.fillna(netflixdata.mean)
+#print(cleaned_data["director"])
+
+#delete all missing value by replacing with next value
+#cleaned_data=netflixdata.fillna(method="bfill")
+#missingval= cleaned_data.isna().sum()
+#print(cleaned_data)
+#print(missingval)
+#print(cleaned_data["director"]#will not work if last value is blank
+
+#delete all missing value by replacing with previous value
+#cleaned_data=netflixdata.fillna(method="ffill")
+#missingval= cleaned_data.isna().sum()
+#print(cleaned_data)
+#print(missingval)
+#print(cleaned_data["director"]) #will not work if first value is blank
+
+#delete all missing value by replacing with next/previous value
+#cleaned_data=netflixdata.fillna(method="ffill").fillna(method="bfill")
+#missingval= cleaned_data.isna().sum()
+#print(cleaned_data)
+#print(missingval)
+#print(cleaned_data["director"])
+
+#drop duplicates
+#drop_duplicates=netflixdata.drop_duplicates()
+#print(netflixdata.shape,drop_duplicates.shape)
+
+#drop_duplicates=netflixdata.drop_duplicates(subset=["type"])
+#print(netflixdata.shape,drop_duplicates.shape)
+
+#drop_duplicates=netflixdata.drop_duplicates(subset=["type","director","cast"])
+#print(netflixdata.shape,drop_duplicates.shape)
+
+print(netflixdata.duplicated())
